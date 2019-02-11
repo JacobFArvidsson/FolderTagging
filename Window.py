56c36ce8b5ad1013b59c69cf9c8dfe3ffe7fda5b
JacobFArvidsson <jacobfarvidsson@gmail.com>
2019-02-11 15:34:22 +0100

    try to add files through pycharm

---
+++ Window.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import QtWidgets
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QWidget()
+    window.setWindowTitle("APA")
+    window.setGeometry(400,50,400,400)
+    window.show()
+    sys.exc_info(app.exec_())
+
+window()
